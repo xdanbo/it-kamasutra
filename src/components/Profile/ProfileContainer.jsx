661246903f38be343475cdc099c1bd92bbca62b7
xdanbo <artemp.mail@gmail.com>
2019-12-08 23:24:40 +0200

    Remove the now ignored directory node_modules

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import * as axios from "axios";
 import Profile from "./Profile.jsx";
 import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
 import { setUserProfile } from "../../redux/profile-reducer.js";
 
 class ProfileContainer extends Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then(response => {
         this.props.setUserProfile(response.data);
       });
@@ -23,4 +28,8 @@ const mapStateToProps = state => ({
   userProfile: state.profilePage.userProfile
 });
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+const WithUriDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUserProfile })(
+  WithUriDataContainerComponent
+);
